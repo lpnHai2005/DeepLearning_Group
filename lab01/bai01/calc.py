30429f990dad0dfb649c59aae62404016f590891
HiCyclone <lephannguyenhai@gmail.com>
2026-09-12 23:36:02 +0700

    Tuan01/Bai123/Fixcalc

--- lab01/bai01/calc.py
+++ lab01/bai01/calc.py
@@ -1,5 +1,29 @@
-print("Calculator Basic")
-a = int(input("Moi ban nhap so a: "))
-b = int(input("Moi ban nhap so b: "))
-c = a + b
-print("c = %d + %d = %d" % (a, b, c))
+while True:
+    try:
+        a = float(input('Nhap a: '))
+        break
+    except ValueError:
+        print('Nhap sai! Vui long nhap mot so.')
+
+while True:
+    try:
+        b = float(input('Nhap b: '))
+        break
+    except ValueError:
+        print('Nhap sai! Vui long nhap mot so.')
+
+# Tính toán
+cong = a + b
+tru = a - b
+nhan = a * b
+
+if b != 0:
+    chia = a / b
+else:
+    chia = 'Khong the chia cho 0'
+
+# Xuất dữ liệu
+print(f'{a} + {b} = {cong}')
+print(f'{a} - {b} = {tru}')
+print(f'{a} * {b} = {nhan}')
+print(f'{a} / {b} = {chia}')
